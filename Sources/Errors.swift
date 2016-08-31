1732affa6cc68a746dd2f45edb4099c8cdf6019d
Logan Wright <logan.william.wright@gmail.com>
2016-08-31 15:38:10 -0400

    updating to vapor 17

--- Sources/Errors.swift
+++ Sources/Errors.swift
@@ -1,15 +1,15 @@
 enum BotError: Swift.Error {
-    /*:
-     Expect file in `Config/bot-config.json` that looks like the following
+    /**
+         Expect file in `Config/bot-config.json` that looks like the following
 
-     {
-     "token": "[your - token - here]"
-     }
-     */
+             {
+                 "token": "[your - token - here]"
+             }
+    */
     case missingConfig
 
-    /*:
-     Got an invalid response from RTM api
-     */
+    /**
+        Got an invalid response from RTM api
+    */
     case invalidResponse
 }
