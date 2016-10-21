5ab64cb2d8df9f5407ad2b431560e5d91bbf9c69
Fabio Milano <fabio@touchwonders.com>
2016-10-21 15:50:48 +0200

    Updated Vapor's dependency to major version 1.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SlackBot",
     dependencies: [
-      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18)
+      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
     ],
     exclude: [
         "Images"
